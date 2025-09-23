4a5d046492e899bf0e0d8d0cd71585e7fdf79b08
miguelsarti <miguelsarti.tds2.senai@gmail.com>
2025-09-23 11:42:07 -0300

    need fix: sobre page

--- src/app/sobre/page.jsx
+++ src/app/sobre/page.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./sobre.module.css";
 import Header from "../components/Header/page";
 import Footer from "../components/Footer/page";
+import Link from "next/link";
 
 export default function Sobre() {
   return (
@@ -12,16 +13,24 @@ export default function Sobre() {
         alt="Título do jogo Dead Cells"
         width={400}
         />
-        <h1 className={styles.title}>Sobre mim:</h1>
-
-        <div className={styles.content}>
-            <img
-            src="img/my-profile.jpg"
-            alt="Minha foto"
-            width={300}
-            />
-
+        
+      <div className={styles.content}>
+        <div className={styles.imageProfile}>
+          <img
+            src="./img/my-profile.jpg"
+            alt="Minha foto de perfil"
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.textContent}>
+          <h1 className={styles.title}>Miguel Sarti</h1>
+          <p className={styles.description}>
+            Eu me chamo Miguel Sarti, tenho 17 anos, sou estudante do SESI e SENAI e atualmente estou na turma 2TDS2. Estou no meu segundo ano de desenvolvimento web, e estou muito animado para continuar aprendendo e crescendo nessa área. O que eu mais gosto de fazer é jogar videogame, ir pra academia e passear.
+          </p>
+          <h2 className={styles.motivacional}>"Nunca desista dos seus sonhos, pois cada passo dado é uma conquista rumo ao sucesso!"</h2>
         </div>  
+
+    </div>
         <Footer/>
     </div>
   );
