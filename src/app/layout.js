1b27ff34af4fa6977110b36db6a986414ee97ca5
miguelsarti <miguelsarti.tds2.senai@gmail.com>
2025-10-13 00:24:12 -0300

    refactor: adjust responsivirt from home

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
     return (
-        <html>
+        <html lang="pt-BR">
+            <head>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+            </head>
             <body>{children}</body>
         </html>
     );
